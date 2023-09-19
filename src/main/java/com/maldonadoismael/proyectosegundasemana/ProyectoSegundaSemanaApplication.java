4043a4c06495da23b8560b5ac99baeb9b0d9a11b
Ismael Maldonado <ismaelma1996@gmail.com>
2023-09-19 16:10:23 +0200

    first commit

---
+++ src/main/java/com/maldonadoismael/proyectosegundasemana/ProyectoSegundaSemanaApplication.java
@@ -0,0 +1,13 @@
+package com.maldonadoismael.proyectosegundasemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSegundaSemanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSegundaSemanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maldonadoismael/proyectosegundasemana/ProyectoSegundaSemanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maldonadoismael.proyectosegundasemana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSegundaSemanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
