927a2deda7deedcfc741db4756cb95ba74f1667e
Ismael Maldonado <ismaelma1996@gmail.com>
2023-09-22 10:26:54 +0200

    Configured the project to autoinsert two records - Create - Drop is added to the properties

--- src/main/java/com/maldonadoismael/proyectosegundasemana/ProyectoSegundaSemanaApplication.java
+++ src/main/java/com/maldonadoismael/proyectosegundasemana/ProyectoSegundaSemanaApplication.java
@@ -1,13 +1,30 @@
 package com.maldonadoismael.proyectosegundasemana;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.maldonadoismael.proyectosegundasemana.data.model.Client;
+import com.maldonadoismael.proyectosegundasemana.data.services.client.ClientService;
+
 @SpringBootApplication
-public class ProyectoSegundaSemanaApplication {
+public class ProyectoSegundaSemanaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSegundaSemanaApplication.class, args);
+	}
+
+	@Autowired
+	private ClientService clientService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Client carlos = new Client("Carlos", "Arranz");
+		Client isabel = new Client("Isabel", "Sinensia");
+
+		clientService.saveNewClient(carlos);
+		clientService.saveNewClient(isabel);
 	}
 
 }
